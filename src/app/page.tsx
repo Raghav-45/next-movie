84794942614a6cc8c7ef51c6a207c9dc1e5cf43f
Aditya <raghavbhai4545@gmail.com>
2024-04-10 21:48:25 +0530

    Movie Name with Date string

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -374,16 +374,41 @@ const homeData = {
   total_results: 869753,
 }
 
+const textDate = async (givenDate: string) => {
+  // Split the input date string into year, month, and day parts
+  const formattedDate: unknown[] = givenDate.split('-')
+
+  // Extract year, month, and day from the parts
+  const year = formattedDate[0] as number
+  const month = formattedDate[1] as number
+  const date = formattedDate[2] as number
+
+  // Array of month names
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  // Return the formatted date string
+  return `${monthNames[month - 1]} ${year}`
+}
+
 export default function Home() {
   return (
     <>
       <VirtualHubSection>
         <CarouselContent className="-ml-3">
           {Array.from({ length: 15 }).map((_, index) => (
-            <CarouselItem
-              key={index}
-              className="pl-3 w-full h-full basis-[15%]"
-            >
+            <CarouselItem key={index} className="pl-3 basis-[15%]">
               <div className="bg-blue-400 rounded-md w-full overflow-hidden aspect-[2/3]">
                 <img
                   className="w-full h-full"
@@ -398,22 +423,23 @@ export default function Home() {
       <VirtualHubSection>
         <CarouselContent className="-ml-3">
           {homeData.results.map((elem, index) => (
-            <CarouselItem
-              key={index}
-              className="pl-3 w-full h-full basis-[15%]"
-            >
-              <div className="flex bg-blue-400 rounded-md overflow-hidden aspect-[2/3]">
+            <CarouselItem key={index} className="pl-3 basis-[15%]">
+              <div className="overflow-hidden">
                 <img
-                  className="w-full h-full"
+                  className="rounded-md w-full h-full overflow-hidden aspect-[2/3]"
                   src={`https://image.tmdb.org/t/p/original${elem.poster_path}`}
                 />
+                <p className="px-1 text-sm truncate">{elem.title}</p>
+                <span className="px-1 text-xs truncate">
+                  {textDate(elem.release_date)}
+                </span>
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
       </VirtualHubSection>
 
-      <section className="gap-6 grid px-4 md:px-6 py-6 w-full container">
+      <section className="gap-6 grid px-4 md:px-6 py-6 w-full">
         <div className="space-y-4">
           <h2 className="font-bold text-2xl md:text-3xl tracking-tight">
             Action & Adventure
